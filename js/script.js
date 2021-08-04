29b5e485de8c7e978628beb8dd830bc42b492bc3
KelbyNylander <kelbyn13@yahoo.com>
2021-08-04 12:36:05 -0400

    updated and working print to page

--- js/script.js
+++ js/script.js
@@ -17,29 +17,31 @@ function showPage(list, page) {
    // create two variables which will represent the index for the first and last student on the page
    let firstStudent = data[0];
    let lastStudent = data[data.length - 1];
+   let startIndex = (page * 9) - 9;
+   let endIndex = (page * 9) - 1;
 
   // select the element with a class of `student-list` and assign it to a variable
   const studentList = document.querySelector('ul');
 
   // set the innerHTML property of the variable you just created to an empty string
    studentList.innerHTML = '';
-   console.log(list);
-   console.log(page);
   // loop over the length of the `list` parameter
   for (i = 0; i < list.length; i++) {
     // inside the loop create a conditional to display the proper students
-    if (i < data.length) {
-      const li = document.createElement('li');
-      li.innerHTML = `<li class="student-item cf">
+    if (i >= startIndex && i <= endIndex) { //might need to change condditonal to i <= startIndex && i >= endIndex
+      const studentItem = document.createElement('li');
+      studentItem.innerHTML = ` <li class="student-item cf">
       <div class="student-details">
-        <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-        <h3>Ethel Dean</h3>
-        <span class="email">ethel.dean@example.com</span>
+        <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+        <h3>${list[i].name.first} ${list[i].name.last}</h3>
+        <span class="email">${list[i].email}</span>
       </div>
       <div class="joined-details">
-        <span class="date">Joined 12-15-2005</span>
+        <span class="date">${list[i].registered.date}</span>
       </div>
-    </li>`
+    </li>`;
+      studentList.appendChild(studentItem);
+      //studentList.insertAdjacentHTML('beforeend', studentItem);
     }
       // inside the conditional:
         // create the elements needed to display the student information
@@ -48,8 +50,7 @@ function showPage(list, page) {
 }
 showPage(data, 1);
 
-let startIndex = (page * 9) - 9;
-let endIndex = (page * 9) - 1;
+
 
 /*
 Create the `addPagination` function
